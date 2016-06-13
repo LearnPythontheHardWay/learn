d3af0a1f3afc8f4b5ac2f0e4e0f9f885445bb227
awthink <github.hihr@yidie.org>
2016-06-14 00:13:22 +0800

    ex1 - ex4

---
+++ ex3+.py
@@ -0,0 +1,33 @@
+# dou shan kai, wo yao kai shi zhuang B le
+print "I will now count my chickens:"
+
+# "Hens" te me shi sha zi yi si? fan zhenng jiu shi ji suan hou mian de suan shi
+# % he JS yi yang, qu mo
+print "Hens", 25 + 30 / 6
+print "Roosters", 100 - 25 * 3 % 4
+
+# dou shan kai, wo yao kai shi bao lu nian ling le
+print "Now I will count the eggs:"
+
+# te me wo cai 7 sui, jian zhi niu B
+print 3+2+1-5+4%2-1/4+6
+
+# zhe ge suan shi zheng que ma
+print "Is it true that 3+2<5-7 ?"
+
+# ji suan, shu chu True or False
+print 3+2<5-7
+
+# lan de xie le.
+# jia fa yun suan de shu zhi
+# bi jiao yun suan de True or False.
+print "What is 3+2 ?", 3+2
+print "What is 5-7 ?", 5-7
+
+print "Oh, that's why it's False."
+
+print "How about some more."
+
+print "Is it greater?", 5>-2
+print "Is it greater or equal?", 5>=-2
+print "Is it less or equal?", 5<=-2
